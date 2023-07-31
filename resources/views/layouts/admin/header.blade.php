f407773206b3e8a750ffe760d0e647e7b5c93fbf
wahjoe16 <wahjoe16@gmail.com>
2023-07-31 11:05:24 +0700

    update halaman register dan login dengan integrasi argon template

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -19,7 +19,7 @@
                 <li class="nav-item dropdown pe-2 d-flex align-items-center">
                     <a href="javascript:;" class="nav-link text-white p-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fa fa-user me-sm-1"></i>
-                        <span class="d-sm-inline d-none">{{ auth()->user()->name }}</span>
+                        <span class="d-sm-inline d-none">{{ auth()->user()->name ?? ''}}</span>
                     </a>
                     <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
                         <li class="mb-2">
